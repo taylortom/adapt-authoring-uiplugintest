a8fcb88c527d3fdeab4997ddad2204f32ac2e223
Tom Taylor <hello@tomtaylor.name>
2021-09-15 13:08:12 +0100

    Fix alert

--- plugins/uiplugintest/index.js
+++ plugins/uiplugintest/index.js
@@ -2,13 +2,13 @@
 define(function(require) {
   var Origin = require('core/origin');
 
-  Origin.on('globalMenu:test:open', () => alert(Origin.l10n.t('app.helloworld')));
+  Origin.on('globalMenu:helloworld:open', () => alert(Origin.l10n.t('app.helloworld')));
 
   Origin.on('origin:dataReady login:changed', function() {
     Origin.globalMenu.addItem({
       "location": "global",
       "text": Origin.l10n.t('app.helloworld'),
-      "icon": "fa-surprise",
+      "icon": "fa-smile-o",
       "callbackEvent": "helloworld:open"
     });
   });
